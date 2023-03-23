fdf5073f27b9a262d5cd7f615b4c05a8a36840a1
deepikagandham <deepikagandham5@gmail.com>
2023-03-23 22:33:20 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def deepu(request):
+    return render(request,'deepu.html')
+    
+def prema(request):
+    return render(request,'prema.html')
